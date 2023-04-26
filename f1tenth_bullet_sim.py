753f7a9781fe1c6fab19f73b69f40d90d08c9f1d
gonultasbu <gonultasbu@users.noreply.github.com>
2023-04-26 12:17:18 -0500

    cleanup unnecessary imports

--- f1tenth_bullet_sim.py
+++ f1tenth_bullet_sim.py
@@ -1,19 +1,17 @@
 import numpy as np
-import time
 import matplotlib
 matplotlib.use('pdf')
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
 from matplotlib import pyplot as plt
 # import scipy.linalg as sp
-import sys, os, fire, math
+import sys, os
 import pybullet
 import seaborn as sns
 from tqdm import tqdm
 from datetime import datetime
 from scipy.spatial.transform import Rotation as R
 from gap_follower import GapFollower
-from scipy.signal import place_poles
 start_time = datetime.now().strftime("%Y-%m-%d%H-%M-%S")
 script_name = os.path.split(sys.argv[0])[-1].split(".")[0]
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
